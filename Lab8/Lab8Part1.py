c575afa09b41e977249c2ae5f57c5a3e1721f254
Rithik Sachdeva <rithiksachdeva@gmail.com>
2023-03-09 20:29:20 -0800

    Changes to Lab8Part1

--- Lab8/Lab8Part1.py
+++ Lab8/Lab8Part1.py
@@ -16,4 +16,35 @@ camera = Camera(flip=0, width=1280, height=800, fps=30)
 while True:
     frame = camera.read()
 
+    params = cv2.SimpleBlobDetector_Params()
+    params.blobColor = 255
+    detector = cv2.SimpleBlobDetector_create(params)
+
+    keypoints = detector.detect(frame)
+    if keypoints: # If blob is seen 
+        for keypoint in keypoints: # for every blob is seen  
+            if keypoint.pt[0] < frame.shape[1] / 3: # exists in 1/3 of frame (left side of frame) 
+                print("RED LED")
+                GPIO.output(11, GPIO.HIGH)
+            elif keypoint.pt[0] < frame.shape[1] * 2 / 3: # exists in 2/3 of frame (right side of frame) 
+                print("GREEN LED")
+                GPIO.output(13, GPIO.HIGH)
+            else: # exists in 3/3 of frame (center of frame) 
+                print("BLUE LED")
+                GPIO.output(15, GPIO.HIGH)
+    else: # no blob 
+        print("NO BLOBS")
+        # Set GPIO pins to low
+
+    # display the frame
+    cv2.imshow('Camera', frame)
+    keyboard = cv2.waitKey(30)
+    if keyboard == 'q' or keyboard == 27:
+        break
+
+camera.release()
+cv2.destroyAllWindows()
+del camera
+print('Stopped')
+
     
