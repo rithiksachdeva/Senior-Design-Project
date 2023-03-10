836a4757bc718c15aa43a6877ba97d8ac3caaf32
Rithik Sachdeva <rithiksachdeva@gmail.com>
2023-03-10 13:49:03 -0800

    Fixed Lab7Part1

--- Lab7/Lab7Part1.py
+++ Lab7/Lab7Part1.py
@@ -16,58 +16,52 @@ mode1 = bus.read_byte_data(address, 0)
 print("Mode1 Reg = ", hex(mode1))
 mode1 = mode1 | 0x10
 bus.write_byte_data(address, 0, mode1)
-bus.write_byte_data(address, 0xFE, 97) # corresponds to 100 Hz
+bus.write_byte_data(address, 0xFE, 62) # corresponds to 100 Hz
 mode1 = mode1 & ~0x10
 bus.write_byte_data(address, 0, mode1)
 time.sleep(1)
+
 mode1 = bus.read_byte_data(address, 0)
 print("Mode1 Reg = ", hex(mode1))
+bus.write_byte_data(address,1,4)
 prescale = bus.read_byte_data(address, 0xFE)
 print("prescale = ", prescale)
+time.sleep(0.5)
 
-def set_pwm(channel, duty_cycle):
-    # Calculate the pulse width based on duty cycle
-    pulse_width = int((duty_cycle / 100) * 4096)
+def setup(channel, offtime):
+    bus.write_byte_data(address,7 + (channel*4),0)
+    bus.write_byte_data(address,6 + (channel*4),0)
+    bus.write_byte_data(address,9 + (channel*4),offtime >> 8)
+    bus.write_byte_data(address,8 + (channel*4),offtime & 0xFF)
 
-    # Set the duty cycle for the given channel
-    off_time = pulse_width & 0xFF
-    on_time = pulse_width >> 8
-    bus.write_byte_data(address, 0x06 + (4 * channel), 0x00)
-    bus.write_byte_data(address, 0x07 + (4 * channel), 0x00)
-    bus.write_byte_data(address, 0x08 + (4 * channel), off_time)
-    bus.write_byte_data(address, 0x09 + (4 * channel), on_time)
+setup(0, 2000)
+setup(1, 1000)
 
-def set_pwm_pulsewidth(channel, pulse_width_ms):
-    bus.write_byte_data(address, 0x06 + 4 * channel, 0)
-    bus.write_byte_data(address, 0x07 + 4 * channel, 0)
-    pulse_width = int(round(pulse_width_ms * 4096 / (1000 * 1000000 / 50)))
-    off_time = pulse_width & 0xFF
-    on_time = pulse_width >> 8
-    bus.write_byte_data(address, 0x08 + 4 * channel, off_time)
-    bus.write_byte_data(address, 0x09 + 4 * channel, on_time)
-
+def updatedutycycle(channel, offtime):
+    bus.write_byte_data(address, 9 + (channel*4), offtime >> 8)
+    bus.write_byte_data(address, 8 + (channel*4), offtime & 0xFF)
 
 while True:
     input1 = GPIO.input(18)
     input2 = GPIO.input(21)
     if input1 == 0 and input2 == 0:
-        set_pwm(1,0)
-        set_pwm_pulsewidth(0,1.5)
-        GPIO.output(12, GPIO.HIGH)
+        updatedutycycle(0, 650)
+        updatedutycycle(1, 0)
+        GPIO.output(ledoutputpin, GPIO.HIGH)
         print("input1 = 0, input2 = 0, output = HIGH")
     elif input1 == 0 and input2 == 1:
-        set_pwm(1,50)
-        set_pwm_pulsewidth(0,1.1)
+        updatedutycycle(0, 480)
+        updatedutycycle(1, 2100)
         GPIO.output(12, GPIO.HIGH)
         print("input1 = 0, input2 = 1, output = HIGH")
     elif input1 == 1 and input2 == 0:
-        set_pwm(1,50)
-        set_pwm_pulsewidth(0,1.9)
+        updatedutycycle(0, 800)
+        updatedutycycle(1, 2100)
         GPIO.output(12, GPIO.HIGH)
         print("input1 = 1, input2 = 0, output = HIGH")
     elif input1 == 1 and input2 == 1:
-        set_pwm(1,80)
-        set_pwm_pulsewidth(0,1.5)
+        updatedutycycle(0, 650)
+        updatedutycycle(1, 3350)
         GPIO.output(12, GPIO.LOW)
         print("input1 = 1, input2 = 1, output = LOW")
     else:
