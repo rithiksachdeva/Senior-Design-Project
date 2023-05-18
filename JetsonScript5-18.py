db0f5c55a4fd81fa64d16e19bcc9bcaee2af6605
Rithik <rithiksachdeva@gmail.com>
2023-05-18 16:38:24 -0700

    Updated

--- JetsonScript5-18.py
+++ JetsonScript5-18.py
@@ -9,11 +9,11 @@ GPIO.setup(18, GPIO.OUT)
 
 # Setup UART
 uart = serial.Serial(
-    port='/dev/ttyTHS1',  # Replace with your UART port
-    baudrate=115200,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE,
-    bytesize=serial.EIGHTBITS
+	port='/dev/ttyTHS1',  # Replace with your UART port
+	baudrate=115200,
+	parity=serial.PARITY_NONE,
+	stopbits=serial.STOPBITS_ONE,
+	bytesize=serial.EIGHTBITS
 )
 
 # Initialize GPIO
@@ -22,20 +22,23 @@ GPIO.output(18, GPIO.LOW)
 
 # Function to handle alternating GPIO states
 def handle_gpio_states():
-    if GPIO.input(21) == GPIO.LOW and GPIO.input(18) == GPIO.LOW:
-        GPIO.output(21, GPIO.HIGH)
-        GPIO.output(18, GPIO.LOW)
-    elif GPIO.input(21) == GPIO.HIGH and GPIO.input(18) == GPIO.LOW:
-        GPIO.output(21, GPIO.LOW)
-        GPIO.output(18, GPIO.HIGH)
-    elif GPIO.input(21) == GPIO.LOW and GPIO.input(18) == GPIO.HIGH:
-        GPIO.output(21, GPIO.LOW)
-        GPIO.output(18, GPIO.LOW)
+	if GPIO.input(21) == GPIO.LOW and GPIO.input(18) == GPIO.LOW:
+		GPIO.output(21, GPIO.HIGH)
+		GPIO.output(18, GPIO.LOW)
+	elif GPIO.input(21) == GPIO.HIGH and GPIO.input(18) == GPIO.LOW:
+		GPIO.output(21, GPIO.LOW)
+		GPIO.output(18, GPIO.HIGH)
+	elif GPIO.input(21) == GPIO.LOW and GPIO.input(18) == GPIO.HIGH:
+		GPIO.output(21, GPIO.LOW)
+		GPIO.output(18, GPIO.LOW)
 
 # Main loop
 while True:
-    if uart.in_waiting > 0:
-        data = uart.read_until().decode('utf-8')
-        if 'FND' in data:
-            handle_gpio_states()
-            uart.write('ACK'.encode())
+	if uart.in_waiting > 0:
+		print("Data Recv")
+		data = uart.read_until().decode('utf-8')
+		print(data == "FND\n")
+		if data == "FND\n":
+			handle_gpio_states()
+			print(GPIO.input(21), GPIO.input(18))			
+			uart.write('ACK'.encode())
