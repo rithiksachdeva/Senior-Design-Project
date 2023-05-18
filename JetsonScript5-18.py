969380e1f53fbbc4101852ba6513cbbcd5b53e1b
Rithik Sachdeva <rithiksachdeva@gmail.com>
2023-05-18 14:16:38 -0700

    Jetson Script for Motor Control Checkoff

---
+++ JetsonScript5-18.py
@@ -0,0 +1,41 @@
+import time
+import serial
+import RPi.GPIO as GPIO
+
+# Setup GPIO
+GPIO.setmode(GPIO.BOARD)  
+GPIO.setup(21, GPIO.OUT)  
+GPIO.setup(18, GPIO.OUT)  
+
+# Setup UART
+uart = serial.Serial(
+    port='/dev/ttyTHS1',  # Replace with your UART port
+    baudrate=115200,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS
+)
+
+# Initialize GPIO
+GPIO.output(21, GPIO.LOW)
+GPIO.output(18, GPIO.LOW)
+
+# Function to handle alternating GPIO states
+def handle_gpio_states():
+    if GPIO.input(21) == GPIO.LOW and GPIO.input(18) == GPIO.LOW:
+        GPIO.output(21, GPIO.HIGH)
+        GPIO.output(18, GPIO.LOW)
+    elif GPIO.input(21) == GPIO.HIGH and GPIO.input(18) == GPIO.LOW:
+        GPIO.output(21, GPIO.LOW)
+        GPIO.output(18, GPIO.HIGH)
+    elif GPIO.input(21) == GPIO.LOW and GPIO.input(18) == GPIO.HIGH:
+        GPIO.output(21, GPIO.LOW)
+        GPIO.output(18, GPIO.LOW)
+
+# Main loop
+while True:
+    if uart.in_waiting > 0:
+        data = uart.read_until().decode('utf-8')
+        if 'FND' in data:
+            handle_gpio_states()
+            uart.write('ACK'.encode())
