42baa62c6adf712826530ef918f02336eb717ee9
Rithik Sachdeva <rithiksachdeva@gmail.com>
2023-06-02 12:16:14 -0700

    new script

--- Final Scripts/JetsonUART.py
+++ Final Scripts/JetsonUART.py
@@ -1,19 +1,39 @@
+##import serial
+##
+##ser_openMV1 = serial.Serial(
+##    port="/dev/ttyTHS1",
+##    baudrate=115200,
+##    bytesize=serial.EIGHTBITS,
+##    parity=serial.PARITY_NONE,
+##    stopbits=serial.STOPBITS_ONE,
+##)
+##
+##while True:
+##    print("Listening...")
+##    if ser_openMV1.in_waiting > 0:
+##        msg = ser_openMV1.readline().decode('utf-8').strip()
+##        if msg == "ready to proceed":
+##            break
+##        else:
+##            print("Unexpected message: ", msg)
+##print("We did it")
+##
 import serial
+import time
 
-ser_openMV1 = serial.Serial(
-    port="/dev/ttyTHS1",
+# Initialize the UART port
+ser = serial.Serial(
+    port="/dev/ttyTHS1",  # Replace with your UART port
     baudrate=115200,
     bytesize=serial.EIGHTBITS,
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE,
 )
 
+time.sleep(1)
+
+# Continuously send data
 while True:
-    print("Listening...")
-    if ser_openMV1.in_waiting > 0:
-        msg = ser_openMV1.readline().decode('utf-8').strip()
-        if msg == "ready to proceed":
-            break
-        else:
-            print("Unexpected message: ", msg)
-print("We did it")
+    message = "Hello from Jetson!\n"
+    ser.write(message.encode())  # Encode the string to bytes and send
+    time.sleep(5)  # Wait for a second
